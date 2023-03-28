bb6185113362676937a5f26126959055686a3319
pyong <58324470+pyong-1459@users.noreply.github.com>
2023-03-28 09:12:43 +0900

    Update tp_mem.v

--- iverilog_dct/tp_mem.v
+++ iverilog_dct/tp_mem.v
@@ -38,81 +38,15 @@ always @ (posedge clk) begin
     end
 end
 
+integer i, j;    
+    
 always @ (posedge clk) begin
     if (rst) begin
-        data[0][0] <= 0;
-        data[1][0] <= 0;
-        data[2][0] <= 0;
-        data[3][0] <= 0;
-        data[4][0] <= 0;
-        data[5][0] <= 0;
-        data[6][0] <= 0;
-        data[7][0] <= 0;
-        data[0][1] <= 0;
-        data[1][1] <= 0;
-        data[2][1] <= 0;
-        data[3][1] <= 0;
-        data[4][1] <= 0;
-        data[5][1] <= 0;
-        data[6][1] <= 0;
-        data[7][1] <= 0;
-        data[0][2] <= 0;
-        data[1][2] <= 0;
-        data[2][2] <= 0;
-        data[3][2] <= 0;
-        data[4][2] <= 0;
-        data[5][2] <= 0;
-        data[6][2] <= 0;
-        data[7][2] <= 0;
-        data[0][3] <= 0;
-        data[1][3] <= 0;
-        data[2][3] <= 0;
-        data[3][3] <= 0;
-        data[4][3] <= 0;
-        data[5][3] <= 0;
-        data[6][3] <= 0;
-        data[7][3] <= 0;
-        data[0][4] <= 0;
-        data[1][4] <= 0;
-        data[2][4] <= 0;
-        data[3][4] <= 0;
-        data[4][4] <= 0;
-        data[5][4] <= 0;
-        data[6][4] <= 0;
-        data[7][4] <= 0;
-        data[0][5] <= 0;
-        data[1][5] <= 0;
-        data[2][5] <= 0;
-        data[3][5] <= 0;
-        data[4][5] <= 0;
-        data[5][5] <= 0;
-        data[6][5] <= 0;
-        data[7][5] <= 0;
-        data[0][6] <= 0;
-        data[1][6] <= 0;
-        data[2][6] <= 0;
-        data[3][6] <= 0;
-        data[4][6] <= 0;
-        data[5][6] <= 0;
-        data[6][6] <= 0;
-        data[7][6] <= 0;
-        data[0][7] <= 0;
-        data[1][7] <= 0;
-        data[2][7] <= 0;
-        data[3][7] <= 0;
-        data[4][7] <= 0;
-        data[5][7] <= 0;
-        data[6][7] <= 0;
-        data[7][7] <= 0;
-        // data[0][8] <= 8'b0;
-        // data[1][8] <= 8'b0;
-        // data[2][8] <= 8'b0;
-        // data[3][8] <= 8'b0;
-        // data[4][8] <= 8'b0;
-        // data[5][8] <= 8'b0;
-        // data[6][8] <= 8'b0;
-        // data[7][8] <= 8'b0;
-        // data[8][8] <= 8'b0;
+        for (i=0;i<8;i=i+1) begin
+            for (j=0;j<8;j=j+1) begin
+                data[j][i] <= 0;
+            end
+        end
     end
     else if (odd) begin
         data[cnt_row][cnt_col] <= mem_in;
